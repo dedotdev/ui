a929d0ddcbbd665947fe56b80c5a6cdec706a310
Thang X. Vu <zthangxv@gmail.com>
2025-02-27 11:39:42 +0700

    chore: add build config & license

---
+++ scripts/copy-files-to-dist.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs';
+import * as path from 'path';
+
+const filesToCopy = ['package.json', 'README.md', 'LICENSE'];
+const targetDir = 'dist';
+
+const main = () => {
+  if (!fs.existsSync(targetDir)) {
+    return;
+  }
+
+  const currentDir = process.cwd();
+
+  filesToCopy.forEach((file) => {
+    let filePath = path.resolve(currentDir, file);
+
+    if (!fs.existsSync(filePath)) {
+      return;
+    }
+
+    let fileContent = fs.readFileSync(filePath, { encoding: 'utf8' });
+
+    if (file === 'package.json') {
+      const pkgJson = JSON.parse(fileContent);
+      pkgJson.main = '';
+      pkgJson.module = './index.js';
+      pkgJson.types = './index.d.ts';
+
+      fileContent = JSON.stringify(pkgJson, null, 2);
+    }
+
+    fs.writeFileSync(path.join(currentDir, targetDir, file), fileContent);
+  });
+};
+
+main();
